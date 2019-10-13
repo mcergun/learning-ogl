1b57541b41a09746236a80cf5ebc28a458522b44
Mert Can Ergun <mcergun90@gmail.com>
2019-10-13 23:42:59 +0300

    1.3 Hello Window

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
-main:
-	${CXX} -o main src/main.cpp -Iinclude
+CC=gcc
+CFLAGS=-I. -Iinclude
+LIBS=`pkg-config --libs glfw3 gl` -ldl
+DEPS = hellomake.h
+
+main: dirs glad
+	@${CXX} ${CFLAGS} src/main.cpp obj/glad.o -o bin/main ${LIBS}
+
+glad: dirs
+	@${CXX} ${CFLAGS} -c src/glad/glad.c -o obj/glad.o
+
+dirs:
+	@mkdir -p obj
+	@mkdir -p bin
 
 clean:
-	@rm -f main
+	@rm -rf bin obj
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,67 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+constexpr int WIN_WID = 800;
+constexpr int WIN_HEI = 600;
+
+// callback for window resizes
+void cbFrameBufferSize(GLFWwindow *win, int wid, int hei);
+void ProcessInput(GLFWwindow *win);
+
 int main(int argc, char **argv)
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Required for MAC builds
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow *win = glfwCreateWindow(WIN_WID, WIN_HEI, "Title", NULL, NULL);
+	if (win == NULL)
+	{
+		std::cout << "Nice fail" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(win);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Another nice fail" << std::endl;
+		return -2;
+	}
+
+	glViewport(0, 0, WIN_WID, WIN_HEI);
+	glfwSetFramebufferSizeCallback(win, cbFrameBufferSize);
+
+	while(!glfwWindowShouldClose(win))
+	{
+		// handle input
+		ProcessInput(win);
+		// handle rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// check events and swap the buffers
+		glfwSwapBuffers(win);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
 	std::cout << "Hello world!" << std::endl;
 	return 0;
+}
+
+void cbFrameBufferSize(GLFWwindow *win, int wid, int hei)
+{
+	// update viewport scalings
+	glViewport(0, 0, wid, hei);
+}
+
+void ProcessInput(GLFWwindow *win)
+{
+	if(glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(win, true);
+	}
 }
