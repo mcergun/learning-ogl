a34a335de3b5f1d1c8bf0700bb0f1555379dd233
Mert Can Ergun <mcergun90@gmail.com>
2019-12-18 22:52:23 +0300

    moved level converter code into a function

--- oktan/Logger.cpp
+++ oktan/Logger.cpp
@@ -6,62 +6,44 @@ namespace oktan
     std::shared_ptr<spdlog::logger> Logger::s_CoreLogger;
     std::shared_ptr<spdlog::logger> Logger::s_AppLogger;
 
+    spdlog::level::level_enum ConvertToSpdLevel(LoggerLevel lvl)
+    {
+        spdlog::level::level_enum logLvl;
+        switch (lvl)
+        {
+        case LoggerLevel::Trace:
+            logLvl = spdlog::level::trace;
+            break;
+        case LoggerLevel::Debug:
+            logLvl = spdlog::level::debug;
+            break;
+        case LoggerLevel::Info:
+            logLvl = spdlog::level::info;
+            break;
+        case LoggerLevel::Warning:
+            logLvl = spdlog::level::warn;
+            break;
+        case LoggerLevel::Error:
+            logLvl = spdlog::level::err;
+            break;
+        case LoggerLevel::Critical:
+            logLvl = spdlog::level::critical;
+            break;
+        case LoggerLevel::Off:
+        default:
+            logLvl = spdlog::level::off;
+            break;
+        }
+        return logLvl;
+    }
+
     void Logger::Initialize(LoggerLevel coreLvl, LoggerLevel appLvl)
     {
         s_CoreLogger = spdlog::stdout_color_mt("OKTAN");
         s_AppLogger = spdlog::stdout_color_mt("APP");
-        spdlog::level::level_enum logLvl;
-        switch (coreLvl)
-        {
-        case LoggerLevel::Trace:
-            logLvl = spdlog::level::trace;
-            break;
-        case LoggerLevel::Debug:
-            logLvl = spdlog::level::debug;
-            break;
-        case LoggerLevel::Info:
-            logLvl = spdlog::level::info;
-            break;
-        case LoggerLevel::Warning:
-            logLvl = spdlog::level::warn;
-            break;
-        case LoggerLevel::Error:
-            logLvl = spdlog::level::err;
-            break;
-        case LoggerLevel::Critical:
-            logLvl = spdlog::level::critical;
-            break;
-        case LoggerLevel::Off:
-        default:
-            logLvl = spdlog::level::off;
-            break;
-        }
+        auto logLvl = ConvertToSpdLevel(coreLvl);
         s_CoreLogger->set_level(logLvl);
-        switch (appLvl)
-        {
-        case LoggerLevel::Trace:
-            logLvl = spdlog::level::trace;
-            break;
-        case LoggerLevel::Debug:
-            logLvl = spdlog::level::debug;
-            break;
-        case LoggerLevel::Info:
-            logLvl = spdlog::level::info;
-            break;
-        case LoggerLevel::Warning:
-            logLvl = spdlog::level::warn;
-            break;
-        case LoggerLevel::Error:
-            logLvl = spdlog::level::err;
-            break;
-        case LoggerLevel::Critical:
-            logLvl = spdlog::level::critical;
-            break;
-        case LoggerLevel::Off:
-        default:
-            logLvl = spdlog::level::off;
-            break;
-        }
+        logLvl = ConvertToSpdLevel(appLvl);
         s_AppLogger->set_level(logLvl);
     }
 }
