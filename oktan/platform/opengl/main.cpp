6c380b943ed82253c3f6fd566db9325db9e2e6f0
Mert Can Ergun <mcergun90@gmail.com>
2019-12-30 22:34:54 +0300

    updated main application with new Window class

--- oktan/platform/opengl/main.cpp
+++ oktan/platform/opengl/main.cpp
@@ -1,38 +1,16 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include "OGLBufferLayout.h"
-#include "OGLVertexBuffer.h"
-#include "OGLIndexBuffer.h"
 #include <oktan/Logger.h>
 #include <oktan/Version.h>
+#include <oktan/Buffers.h>
+#include <oktan/Window.h>
 
 int main(int argc, char **argv)
 {
 	oktan::Logger::Initialize(oktan::LoggerLevel::Trace, oktan::LoggerLevel::Trace);
     // drawer.AddVertexAttribute(0, DrawerType_Float, 3, false, 5 * sizeof(float), 0);
 	// drawer.AddVertexAttribute(1, DrawerType_Float, 2, false, 5 * sizeof(float), 3 * sizeof(float));
-    
-    glfwInit();
-	OK_APP_LOG_TRACE("glfw initialized");
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow *win = glfwCreateWindow(600, 480, "Title", NULL, NULL);
-	if (win == NULL)
-	{
-		std::cout << "Nice fail" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(win);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Another nice fail" << std::endl;
-		return -2;
-	}
-	OK_APP_LOG_TRACE("glad loaded");
+    auto win = oktan::Window::Create(600, 480, "Title");
+	win->Open();
 
 	OK_LOG_INFO("Oktan Started: v{}.{}.{}.{}",
 		PROJECT_VERSION_MAJOR,
