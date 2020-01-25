eaeb8dad8a34219cdc1fb98635d48ab7f2377a1f
Mert Can Ergun <mcergun90@gmail.com>
2020-01-26 02:42:07 +0300

    made a moving triangle using uniforms, with my own gl library

--- oktan/platform/opengl/main.cpp
+++ oktan/platform/opengl/main.cpp
@@ -12,30 +12,33 @@
 
 int main(int argc, char **argv)
 {
-	oktan::Logger::Initialize(oktan::LoggerLevel::Trace, oktan::LoggerLevel::Trace);
+	oktan::Logger::Initialize(oktan::LoggerLevel::Info, oktan::LoggerLevel::Info);
     auto win = oktan::Window::Create(600, 480, "Title");
 	win->Open();
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,
+		 0.0f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,
 	};
 
 	auto vao = oktan::VertexArray::Create();
 	vao->AddVertexBuffer(vertices, sizeof(vertices));
 	auto blo = oktan::BufferLayout::Create({
+		{oktan::BaseType::Float, 3, false},
 		{oktan::BaseType::Float, 3, false}
 	});
 
 	auto shader = oktan::Shader::Create("shaders/1.4vtx.glsl", "shaders/1.4frg.glsl");
 	auto scene = oktan::Scene::Create(oktan::DrawPrimitives::Triangles);
 
+	int i = 0;
 	while (!win->ShouldClose())
 	{
 		scene->ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		scene->ClearColorBuffer();
-
+		float val = sin(i++ / 48.0f) / 2;
+		shader->SetUniform("xOffset", val);
 		shader->Use();
 		vao->Bind();
 		scene->DrawArrays(0, 3);
