251ccb8ee378831a2531cf33e54a5dec81cdb7ea
Mert Can Ergun <mcergun90@gmail.com>
2020-01-30 22:07:12 +0300

    hello 3D

--- main.cpp
+++ main.cpp
@@ -39,27 +39,31 @@ int main(int argc, char **argv)
 		{oktan::BaseType::Float, 2, false},
 	});
 
-	auto shader = oktan::Shader::Create("../shaders/1.7vtx.glsl", "../shaders/1.7frg.glsl");
+	auto shader = oktan::Shader::Create("../shaders/1.8vtx.glsl", "../shaders/1.8frg.glsl");
 	auto scene = oktan::Scene::Create(oktan::DrawPrimitives::Triangles);
 	int32_t slot1 = 0;
 	int32_t slot2 = 1;
 	auto tex1 = oktan::Texture::Create("../textures/container.jpg", oktan::TextureType::Texture_2D, oktan::ColorType::Color_RGB, slot1);
 	auto tex2 = oktan::Texture::Create("../textures/awesomeface.png", oktan::TextureType::Texture_2D, oktan::ColorType::Color_RGBA, slot2);
 	int i = 0;
-	glm::mat4 transform = glm::mat4(1.0f);
-	transform = glm::rotate(transform, glm::radians(90.f), glm::vec3(0.0f, 0.0f, 1.0f));
-	transform = glm::scale(transform, glm::vec3(0.5f, 0.5f, 0.5f));
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
 	while (!win->ShouldClose())
 	{
 		scene->ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		scene->ClearColorBuffer();
 		float val = sin(i++ / 72.0f) / 2;
-		transform = glm::mat4(1.0f);
-		transform = glm::rotate(transform, val * 3, glm::vec3(0.0f, 0.0f, 1.0f));
-		transform = glm::scale(transform, glm::vec3(0.5f, 0.5f, 0.5f));
+		model = glm::rotate(model, glm::radians(val * 15), glm::vec3(0.5f, 1.0f, 0.0f));
 		shader->SetUniform("tex1", slot1);
 		shader->SetUniform("tex2", slot2);
-		shader->SetUniform("transform", transform);
+		shader->SetUniform("model", model);
+		shader->SetUniform("view", view);
+		shader->SetUniform("projection", projection);
 		shader->Use();
 		vao->Bind();
 		scene->DrawElements(0, 6, 0);
