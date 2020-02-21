ee12554fa2b108f9ca9ce146673ed402b91e145a
Mert Can Ergun <mcergun@windowslive.com>
2020-02-21 23:27:40 +0300

    added PerspectiveCamera

--- oktan/renderer/Camera.cpp
+++ oktan/renderer/Camera.cpp
@@ -4,6 +4,36 @@
 
 namespace oktan
 {
+	PerspectiveCamera::PerspectiveCamera(float fov, float width, float height, float near, float far)
+		: m_Projection(glm::perspective(glm::radians(fov), width / height, near, far))
+	{
+		m_ViewProjection = m_Projection * m_View;
+		RecalculateView();
+	}
+
+	void PerspectiveCamera::SetProjection(float width, float height, float fov, float near, float far)
+	{
+		m_Projection = glm::perspective(glm::radians(fov), width / height, near, far);
+	}
+
+	void PerspectiveCamera::SetPosition(glm::vec3 &pos)
+	{
+		m_Position = pos;
+		RecalculateView();
+	}
+
+	void PerspectiveCamera::SetTarget(glm::vec3 &target)
+	{
+		m_Target = target;
+		RecalculateView();
+	}
+
+	void PerspectiveCamera::RecalculateView()
+	{
+		m_View = glm::lookAt(m_Position, m_Target, UPVEC);
+		m_ViewProjection = m_Projection * m_View;
+	}
+
 	OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
 		: m_Projection(glm::ortho(left, right, bottom, top))
 	{
--- oktan/renderer/Camera.h
+++ oktan/renderer/Camera.h
@@ -5,6 +5,35 @@
 
 namespace oktan
 {
+	class PerspectiveCamera
+	{
+	public:
+		PerspectiveCamera(float fov, float width, float height, float near, float far);
+		virtual ~PerspectiveCamera() = default;
+
+		glm::mat4 GetProjectionMatrix() const { return m_Projection; }
+		glm::mat4 GetViewMatrix() const { return m_View; }
+		glm::mat4 GetViewProjectionMatrix() const { return m_ViewProjection; }
+		glm::vec3 GetPosition() const { return m_Position; }
+		glm::vec3 GetTarget() const { return m_Target; }
+
+		void SetProjection(float fov, float width, float height, float near, float far);
+		void SetPosition(glm::vec3 &pos);
+		void SetTarget(glm::vec3 &target);
+
+	protected:
+		void RecalculateView();
+
+	private:
+		glm::mat4 m_Projection;
+		glm::mat4 m_View;
+		glm::mat4 m_ViewProjection;
+
+		glm::vec3 m_Position = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 m_Target = { 0.0f, 0.0f, 0.0f };
+		const glm::vec3 UPVEC = { 0.0f, 1.0f, 0.0f };
+	};
+
 	class OrthographicCamera
 	{
 	public:
