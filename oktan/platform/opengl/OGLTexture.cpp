87c13ea3b91439dc070e5612009bbffbc07eb307
Mert Can Ergün <mcergun@windowslive.com>
2020-03-24 02:25:52 +0300

    added controls for grabbing available texture unit count added destructor to OGLTexture class added a contructor to OGLTexture to work without explicity assigning a slot to it. the slot is captured by the class using new functions.

--- oktan/platform/opengl/OGLTexture.cpp
+++ oktan/platform/opengl/OGLTexture.cpp
@@ -8,7 +8,11 @@ namespace oktan
     static GLenum ConvertToGL(ColorType color);
     static GLenum ConvertToGL(TextureType tex);
 
-    OGLTexture::OGLTexture(std::string path, TextureType type, ColorType color, uint32_t slot)
+    std::vector<bool> OGLTexture::s_AvailableSlots = std::vector<bool>(16);
+    int32_t OGLTexture::s_MaxTextureCount = 0;
+
+    OGLTexture::OGLTexture(std::string path, TextureType type, ColorType color, uint32_t slot) :
+        m_Slot(slot)
     {
         m_Type = type;
         m_Color = color;
@@ -20,7 +24,7 @@ namespace oktan
             OK_LOG_TRACE("Created Texture {}, {} dimensions: {} x {}", m_Id, path, m_Width, m_Height);
             GLenum texEnum = ConvertToGL(m_Type);
             GLenum colEnum = ConvertToGL(m_Color);
-            Assign(slot);
+            Assign(m_Slot);
             glTexParameteri(texEnum, GL_TEXTURE_WRAP_S, GL_REPEAT);	
             glTexParameteri(texEnum, GL_TEXTURE_WRAP_T, GL_REPEAT);
             glTexParameteri(texEnum, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -35,6 +39,40 @@ namespace oktan
         }
     }
 
+    OGLTexture::OGLTexture(std::string path, TextureType type, ColorType color)
+    {
+        m_Type = type;
+        m_Color = color;
+        stbi_set_flip_vertically_on_load(true);
+        unsigned char* data = stbi_load(path.c_str(), &m_Width, &m_Height, &m_NumChannels, 0);
+        if (data)
+        {
+            PrepareSlots();
+            m_Slot = GetNextSlot();
+            glGenTextures(1, &m_Id);
+            OK_LOG_TRACE("Created Texture {}, {} dimensions: {} x {}", m_Id, path, m_Width, m_Height);
+            GLenum texEnum = ConvertToGL(m_Type);
+            GLenum colEnum = ConvertToGL(m_Color);
+            Assign(m_Slot);
+            glTexParameteri(texEnum, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(texEnum, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(texEnum, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(texEnum, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexImage2D(texEnum, 0, colEnum, m_Width, m_Height, 0, colEnum, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(texEnum);
+            stbi_image_free(data);
+        }
+        else
+        {
+            OK_LOG_ERROR("Texture can't be loaded from {} : {}", path, stbi_failure_reason());
+        }
+    }
+
+    OGLTexture::~OGLTexture()
+    {
+        glDeleteTextures(1, &m_Id);
+    }
+
     void OGLTexture::Bind()
     {
         GLenum texEnum = ConvertToGL(m_Type);
@@ -47,6 +85,32 @@ namespace oktan
         glActiveTexture(GL_TEXTURE0 + slot);
         OK_LOG_TRACE("Assigned texture: {} to GL_TEXTURE{}", m_Id, slot);
         Bind();
+    }
+
+    int32_t OGLTexture::GetNextSlot()
+    {
+        bool found = false;
+        size_t pos = 0;
+        for (; !found && pos < s_AvailableSlots.size(); pos++)
+        {
+            found = s_AvailableSlots[pos];
+        }
+        if (found)
+        {
+            pos--;
+        }
+        return pos;
+    }
+
+    void OGLTexture::PrepareSlots()
+    {
+        if (s_MaxTextureCount == 0)
+        {
+            glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &s_MaxTextureCount);
+            s_AvailableSlots.reserve(s_MaxTextureCount);
+            s_AvailableSlots.assign(s_MaxTextureCount, true);
+            OK_LOG_INFO("Maximum number of texture units: {}", s_MaxTextureCount);
+        }
     }
 
     GLenum ConvertToGL(ColorType color)
--- oktan/platform/opengl/OGLTexture.h
+++ oktan/platform/opengl/OGLTexture.h
@@ -1,6 +1,7 @@
 #ifndef _OK_OGL_TEXTURE_H_
 #define _OK_OGL_TEXTURE_H_
 
+#include <vector>
 #include <oktan/Core.h>
 #include <oktan/renderer/Texture.h>
 
@@ -10,8 +11,19 @@ namespace oktan
     {
     public:
         OGLTexture(std::string path, TextureType type, ColorType color, uint32_t slot);
+        OGLTexture(std::string path, TextureType type, ColorType color);
+        ~OGLTexture();
         void Bind() override;
         void Assign(uint32_t slot) override;
+
+        static int32_t GetNextSlot();
+        static void PrepareSlots();
+
+    private:
+        static std::vector<bool> s_AvailableSlots;
+        static int32_t s_MaxTextureCount;
+
+        int32_t m_Slot = 0;
     };
 }
 
